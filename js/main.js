323fbebd20434c3d3b90ab193898fb119b2f6642
smartboy0310 <qdustudent6@gmail.com>
2021-09-04 04:40:58 +0500

    Add to html faq section

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const elMain = document.querySelector('.main');
+const elArrow__btn__one = document.querySelector('.arrow__btn__one');
+
+elArrow__btn__one.addEventListener('click', () => {
+	elMain.classList.toggle('btn--open--one');
+});
+const elArrow__btn__two = document.querySelector('.arrow__btn__two');
+
+elArrow__btn__two.addEventListener('click', () => {
+	elMain.classList.toggle('btn--open--two');
+});
+const elArrow__btn__three = document.querySelector('.arrow__btn__three');
+
+elArrow__btn__three.addEventListener('click', () => {
+	elMain.classList.toggle('btn--open--three');
+});
+const elArrow__btn__four = document.querySelector('.arrow__btn__four');
+
+elArrow__btn__four.addEventListener('click', () => {
+	elMain.classList.toggle('btn--open--four');
+});
+const elArrow__btn__five = document.querySelector('.arrow__btn__five');
+
+elArrow__btn__five.addEventListener('click', () => {
+	elMain.classList.toggle('btn--open--five');
+});
+const elArrow__btn__six = document.querySelector('.arrow__btn__six');
+
+elArrow__btn__six.addEventListener('click', () => {
+	elMain.classList.toggle('btn--open--six');
+});
